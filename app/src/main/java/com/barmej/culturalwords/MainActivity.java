22061605be5dac7b333305c137eb87541522f268
Abdullah Alhussainan <young.moby@gmail.com>
2019-10-09 10:50:04 +0300

    Update MainActivity.java
    Permission boiler plate code

--- app/src/main/java/com/barmej/culturalwords/MainActivity.java
+++ app/src/main/java/com/barmej/culturalwords/MainActivity.java
@@ -1,14 +1,105 @@
 package com.barmej.culturalwords;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
     }
+
+        /**
+     *
+     * يجب عليك كتابة الكود الذي يقوم بمشاركة الصيرة في هذه الدالة
+     *
+     */
+    private void shareImage(int source, String title, String description) {
+       // كود مشاركة الصورة
+    }
+
+    /**
+     * هذه الدالة خاصة بطلب الصلاحية WRITE_EXTERNAL_STORAGE الطلوبة لمشاركة الصورة
+     *
+     *  ولمشاركة الصورة تقوم باستدعاء الدالة checkPermissionAndShare للتأكد من الصلاحية ومن ثم مشاركة الصورة
+     *
+     **/
+    private void checkPermissionAndShare() {
+        //      insertImage في النسخ من آندرويد 6 إلى آندرويد 9 يجب طلب الصلاحية لكي نتمكن من استخدام الدالة
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // هنا لا يوجد صلاحية للتطبيق ويجب علينا طلب الصلاحية منه عن طريك الكود التالي
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // بسبب عدم منح المستخدم الصلاحية للتطبيق فمن الأفضل شرح فائدتها له عن طريق عرض رسالة تشرح ذلك
+
+                // هنا نقوم بإنشاء AlertDialog لعرض رسالة تشرح للمستخدم فائدة منح الصلاحية
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setTitle(R.string.permission_title)
+                        .setMessage(R.string.permission_explanation)
+                        .setPositiveButton(R.string.grant, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // requestPermissions عند الضغط على زر منح نقوم بطلب الصلاحية عن طريق الدالة
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                            }
+                        }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //  عند الضغط على زر منع نقوم بإخفاء الرسالة وكأن شيء لم يكن
+                                dialogInterface.dismiss();
+                            }
+                        }).create();
+
+                // نقوم بإظهار الرسالة بعد إنشاء alertDialog
+                alertDialog.show();
+
+            } else {
+                // لا داعي لشرح فائدة الصلاحية للمستخدم ويمكننا طلب الصلاحية منه
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            }
+
+        } else {
+            // الصلاحية ممنوحه مسبقا لذلك يمكننا مشاركة الصورة
+
+            shareImage(R.drawable.gift, "", "");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // يتم استدعاء هذه الدالة بعد اختيار المستخدم احد الخيارين من رسالة طلب الصلاحية
+        if (requestCode == MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // تم منح الصلاحية من قبل المستخدم لذلك يمكننا مشاركة الصورة الآن
+                shareImage(R.drawable.gift, "", "");
+
+            } else {
+                // لم يتم منح الصلاحية من المستخدم لذلك لن نقوم بمشاركة الصورة، طبعا يمكننا تنبيه المستخدم بأنه لن يتم مشاركة الصورة لسبب عدم منح الصلاحية للتطبيق
+                Toast.makeText(MainActivity.this, R.string.permission_explanation, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
 }
