112db90b42470890f6a319ca041677b0cd39e5ff
Nasser Alawi <duniaalahlam2016@gmail.com>
2022-07-28 04:31:23 +0300

    Basic Project

--- app/src/main/java/com/barmej/culturalwords/MainActivity.java
+++ app/src/main/java/com/barmej/culturalwords/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_answer);
+        setContentView(R.layout.activity_main);
     }
 
     /**
